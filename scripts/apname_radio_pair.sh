623d596d9bfa794ef19d8cbfb50b0156e72b76b3
Hirochika Asai <panda@jar.jp>
2015-07-20 10:12:05 +0200

    implemented couting the number of associations by a pair of AP name and radio.

---
+++ scripts/apname_radio_pair.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+# Copyright (c) 2015 Hirochika Asai <asai@jar.jp>
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+if [ -z "$1" -o -z "$2" -o -z "$3" -o -z "$4" ];
+then
+	echo "Usage: $0 host community apname radio(0/1)" >& 2
+	exit -1
+fi;
+
+host=$1
+community=$2
+apname=$3
+radio=$4
+bssid=""
+
+cnt=0
+
+IFS="
+"
+
+for line in `snmpwalk -On -v2c -c "$community" "$host" .1.3.6.1.4.1.9.9.513.1.1.1.1.5 | sed -e 's/\.1\.3\.6\.1\.4\.1\.9\.9\.513\.1\.1\.1\.1\.5\.\([0-9\.]*\).*STRING: \"\(.*\)\"/\1 \2/'`;
+do
+	IFS=" "
+	set $line
+	suffix=$1
+	myapname=$2
+	if [ "$myapname" == "$apname" ];
+	then
+		bssid=$suffix
+	fi
+done
+
+IFS="
+"
+for line in `snmpwalk -Ox -On -v2c -c "$community" "$host" .1.3.6.1.4.1.9.9.599.1.3.1.1.8 \
+	| sed -e 's/\.1\.3\.6\.1\.4\.1\.9\.9\.599\.1\.3\.1\.1\.8\([0-9\.]*\).*STRING: \(.*\)/\1 \2/'`;
+do
+	IFS=" "
+	set $line
+	suffix=$1
+	oid_suffix=`printf "%d.%d.%d.%d.%d.%d\n" "0x$2" "0x$3" "0x$4" "0x$5" "0x$6" "0x$7"`
+	if [ "$oid_suffix" == "$bssid" ];
+	then
+		oid=".1.3.6.1.4.1.14179.2.1.4.1.25$suffix"
+		protocol=`snmpwalk -On -v2c -c "$community" "$host" $oid | sed -e 's/.*INTEGER: \(.*\)/\1/'`
+		if [ $protocol -eq 2 -o $protocol -eq 3 -o $protocol -eq 6 ];
+		then
+			myradio=0
+		else
+			myradio=1
+		fi
+		if [ $radio -eq $myradio ];
+		then
+			cnt=`expr $cnt + 1`
+		fi
+	fi
+done
+echo $cnt
+
